e1b6fe970f77c0f2f6e9922dd926a0ade8f14f3a
haefneryan <haefner.ryan@gmail.com>
2022-07-07 09:07:25 -0400

    Code clean up

--- soccerScores.js
+++ soccerScores.js
@@ -4,144 +4,201 @@ let dataLoaded = false;
 
 //Function to populate date input date with today's date
 function getDate() {
-    let dd = today.getDate();
-    let mm = today.getMonth()+1; //January is 0!
-    let yyyy = today.getFullYear();
-  
-    if(dd<10) {
-        dd = '0'+dd
-    } 
-  
-    if(mm<10) {
-        mm = '0'+mm
-    } 
-  
-    today = yyyy + '-' + mm + '-' + dd;
-    date = today;
-    document.getElementById("date").value = today;
+  let dd = today.getDate();
+  let mm = today.getMonth() + 1; //January is 0!
+  let yyyy = today.getFullYear();
+
+  if (dd < 10) {
+    dd = "0" + dd;
+  }
+
+  if (mm < 10) {
+    mm = "0" + mm;
+  }
+
+  today = yyyy + "-" + mm + "-" + dd;
+  date = today;
+  document.getElementById("date").value = today;
 }
 
-function loadData(){
-    console.log(today);
-    let request = new XMLHttpRequest();
+function loadData() {
+  let request = new XMLHttpRequest();
 
-    //Sets API URL, API token, and data includes
-    let api_token = 'CHPO1UMSVjcgZA8oI6xmeLIxzAtVDXeiY6fypOF7npPkB7oqvjniWkg5Z2np';
-    let includes = '&include=localTeam,visitorTeam,events,league';
-    let url = 'https://soccer.sportmonks.com/api/v2.0/fixtures/date/' + date + '?api_token=' + api_token + includes;
+  //Sets API URL, API token, and data includes
+  let api_token =
+    "CHPO1UMSVjcgZA8oI6xmeLIxzAtVDXeiY6fypOF7npPkB7oqvjniWkg5Z2np";
+  let includes = "&include=localTeam,visitorTeam,events,league";
+  let url =
+    "https://soccer.sportmonks.com/api/v2.0/fixtures/date/" +
+    date +
+    "?api_token=" +
+    api_token +
+    includes;
 
-    request.open('GET', url, true);
-    let matchUp = [];
+  request.open("GET", url, true);
+  let matchUp = [];
 
-    request.onload = function() {
-        let response = JSON.parse(this.response);
-        dataLoaded = true;
-        console.log(response);
+  request.onload = function () {
+    let response = JSON.parse(this.response);
+    dataLoaded = true;
 
-        //Parses response data into array
-        let x = response.data.length;
-        for(let i=0; i<x; i++){
-            matchUp[i] = [
-                JSON.stringify(response.data[i].league.data.name),
-                JSON.stringify(response.data[i].localTeam.data.name),
-                JSON.stringify(response.data[i].scores.localteam_score),
-                JSON.stringify(response.data[i].scores.visitorteam_score),
-                JSON.stringify(response.data[i].visitorTeam.data.name),
-                JSON.stringify(response.data[i].league.data.logo_path),
-                JSON.stringify(response.data[i].localTeam.data.logo_path),
-                JSON.stringify(response.data[i].visitorTeam.data.logo_path)
-            ];  
-        }
-
-        //Removes "" from strings in array
-        if(x != 0 ){
-            let y = matchUp[0].length;
-            for(let j=0; j<x; j++){
-                for(let k=0; k<y; k++){
-                    matchUp[j][k] = matchUp[j][k].replace(/['"]+/g, '');
-                }
-            }
-        }
-
-        console.log(matchUp);
-        console.log(dataLoaded);
-
-        //Loops through array to populate Scottish Premiership Scores
-        let scottishPremiershipCount = 0;
-        for(let j=0; j<x; j++){
-            if(matchUp[j][0] == 'Premiership'){
-                document.getElementById('scottishPremiershipLogo').src = matchUp[j][5];
-                document.getElementById('scottishPremiershipName').innerHTML = 'Scottish ' + matchUp[j][0];
-                document.getElementById('scottishPremiership').innerHTML += "<br><div id=matchUp_'" + j + "' class='matchUp'><div class='home'><img src=" + matchUp[j][6] + " class='teamLogoHome'><div id='homeTeam_" + j + "' class='homeTeam'>" + matchUp[j][1] + "</div><div id='homeTeamScore_" + j + "'class='val score'>" + matchUp[j][2] + "</div></div><div class='val' id='dash'>-</div><div class='away'><div id='awayTeamScore_" + j + "' class='val score'>" + matchUp[j][3] + "</div><div id='awayTeam_" + j + "' class='awayTeam'>" + matchUp[j][4] + "</div><img src=" + matchUp[j][7] + " class='teamLogoAway'></div></div>" 
-                scottishPremiershipCount++;
-            }
-        }
-
-        if (scottishPremiershipCount == 0) {
-            document.getElementById('scottishPremiership').innerHTML = '<br>No Games';
-        }
-
-        //Loops through array to populate Danish Superliga Scores
-        let superligaCount = 0;
-        for(let j=0; j<x; j++){
-            if(matchUp[j][0] == 'Superliga'){
-                document.getElementById('superligaLogo').src = matchUp[j][5];
-                document.getElementById('superligaName').innerHTML = 'Danish ' + matchUp[j][0];
-                document.getElementById('superliga').innerHTML += "<br><div id=matchUp_'" + j + "' class='matchUp'><div class='home'><img src=" + matchUp[j][6] + " class='teamLogoHome'><div id='homeTeam_" + j + "' class='homeTeam'>" + matchUp[j][1] + "</div><div id='homeTeamScore_" + j + "'class='val score'>" + matchUp[j][2] + "</div></div><div class='val' id='dash'>-</div><div class='away'><div id='awayTeamScore_" + j + "' class='val score'>" + matchUp[j][3] + "</div><div id='awayTeam_" + j + "' class='awayTeam'>" + matchUp[j][4] + "</div><img src=" + matchUp[j][7] + " class='teamLogoAway'></div></div>" 
-                superligaCount++;
-            }
-        }
-
-        if (superligaCount == 0) {
-            document.getElementById('superliga').innerHTML = '<br>No Games';
-        }
-
-        //Styles score background depending on result
-        for(let i=0; i<x; i++){
-            if(matchUp[i][2] > matchUp[i][3]){
-                document.getElementById('homeTeamScore_' + i).style.backgroundColor = 'green';
-                document.getElementById('awayTeamScore_' + i).style.backgroundColor = 'red';
-            }
-            if(matchUp[i][2] < matchUp[i][3]){
-                document.getElementById('homeTeamScore_' + i).style.backgroundColor = 'red';
-                document.getElementById('awayTeamScore_' + i).style.backgroundColor = 'green';
-            }
-            if(matchUp[i][2] == matchUp[i][3]){
-                document.getElementById('homeTeamScore_' + i).style.backgroundColor = 'grey';
-                document.getElementById('awayTeamScore_' + i).style.backgroundColor = 'grey';
-            }
-        }
-
-        //Populate if no games are played on selected day
-        if(x == 0) {
-            document.getElementById('scottishPremiership').innerHTML = '<br>No Games<br>';
-            document.getElementById('superliga').innerHTML = '<br>No Games<br>';
-        }
-
+    //Parses response data into array
+    let x = response.data.length;
+    for (let i = 0; i < x; i++) {
+      matchUp[i] = [
+        JSON.stringify(response.data[i].league.data.name),
+        JSON.stringify(response.data[i].localTeam.data.name),
+        JSON.stringify(response.data[i].scores.localteam_score),
+        JSON.stringify(response.data[i].scores.visitorteam_score),
+        JSON.stringify(response.data[i].visitorTeam.data.name),
+        JSON.stringify(response.data[i].league.data.logo_path),
+        JSON.stringify(response.data[i].localTeam.data.logo_path),
+        JSON.stringify(response.data[i].visitorTeam.data.logo_path),
+      ];
     }
-    request.send();
+
+    //Removes "" from strings in array
+    if (x != 0) {
+      let y = matchUp[0].length;
+      for (let j = 0; j < x; j++) {
+        for (let k = 0; k < y; k++) {
+          matchUp[j][k] = matchUp[j][k].replace(/['"]+/g, "");
+        }
+      }
+    }
+
+    //Loops through array to populate Scottish Premiership Scores
+    let scottishPremiershipCount = 0;
+    for (let j = 0; j < x; j++) {
+      if (matchUp[j][0] == "Premiership") {
+        document.getElementById("scottishPremiershipLogo").src = matchUp[j][5];
+        document.getElementById("scottishPremiershipName").innerHTML =
+          "Scottish " + matchUp[j][0];
+        document.getElementById("scottishPremiership").innerHTML +=
+          "<br><div id=matchUp_'" +
+          j +
+          "' class='matchUp'><div class='home'><img src=" +
+          matchUp[j][6] +
+          " class='teamLogoHome'><div id='homeTeam_" +
+          j +
+          "' class='homeTeam'>" +
+          matchUp[j][1] +
+          "</div><div id='homeTeamScore_" +
+          j +
+          "'class='val score'>" +
+          matchUp[j][2] +
+          "</div></div><div class='val' id='dash'>-</div><div class='away'><div id='awayTeamScore_" +
+          j +
+          "' class='val score'>" +
+          matchUp[j][3] +
+          "</div><div id='awayTeam_" +
+          j +
+          "' class='awayTeam'>" +
+          matchUp[j][4] +
+          "</div><img src=" +
+          matchUp[j][7] +
+          " class='teamLogoAway'></div></div>";
+        scottishPremiershipCount++;
+      }
+    }
+
+    if (scottishPremiershipCount == 0) {
+      document.getElementById("scottishPremiership").innerHTML = "<br>No Games";
+    }
+
+    //Loops through array to populate Danish Superliga Scores
+    let superligaCount = 0;
+    for (let j = 0; j < x; j++) {
+      if (matchUp[j][0] == "Superliga") {
+        document.getElementById("superligaLogo").src = matchUp[j][5];
+        document.getElementById("superligaName").innerHTML =
+          "Danish " + matchUp[j][0];
+        document.getElementById("superliga").innerHTML +=
+          "<br><div id=matchUp_'" +
+          j +
+          "' class='matchUp'><div class='home'><img src=" +
+          matchUp[j][6] +
+          " class='teamLogoHome'><div id='homeTeam_" +
+          j +
+          "' class='homeTeam'>" +
+          matchUp[j][1] +
+          "</div><div id='homeTeamScore_" +
+          j +
+          "'class='val score'>" +
+          matchUp[j][2] +
+          "</div></div><div class='val' id='dash'>-</div><div class='away'><div id='awayTeamScore_" +
+          j +
+          "' class='val score'>" +
+          matchUp[j][3] +
+          "</div><div id='awayTeam_" +
+          j +
+          "' class='awayTeam'>" +
+          matchUp[j][4] +
+          "</div><img src=" +
+          matchUp[j][7] +
+          " class='teamLogoAway'></div></div>";
+        superligaCount++;
+      }
+    }
+
+    if (superligaCount == 0) {
+      document.getElementById("superliga").innerHTML = "<br>No Games";
+    }
+
+    //Styles score background depending on result
+    for (let i = 0; i < x; i++) {
+      if (matchUp[i][2] > matchUp[i][3]) {
+        document.getElementById("homeTeamScore_" + i).style.backgroundColor =
+          "green";
+        document.getElementById("awayTeamScore_" + i).style.backgroundColor =
+          "red";
+      }
+      if (matchUp[i][2] < matchUp[i][3]) {
+        document.getElementById("homeTeamScore_" + i).style.backgroundColor =
+          "red";
+        document.getElementById("awayTeamScore_" + i).style.backgroundColor =
+          "green";
+      }
+      if (matchUp[i][2] == matchUp[i][3]) {
+        document.getElementById("homeTeamScore_" + i).style.backgroundColor =
+          "grey";
+        document.getElementById("awayTeamScore_" + i).style.backgroundColor =
+          "grey";
+      }
+    }
+
+    //Populate if no games are played on selected day
+    if (x == 0) {
+      document.getElementById("scottishPremiership").innerHTML =
+        "<br>No Games<br>";
+      document.getElementById("superliga").innerHTML = "<br>No Games<br>";
+    }
+  };
+  request.send();
 }
 
 function submitchange() {
-    
-    //Clears out what was in score divs
-    document.getElementById('scottishPremiership').innerHTML = '';
-    document.getElementById('superliga').innerHTML = '';
+  //Clears out what was in score divs
+  document.getElementById("scottishPremiership").innerHTML = "";
+  document.getElementById("superliga").innerHTML = "";
 
-    //Turn off and on displaying scores depending on user input
-    if(document.getElementById('league').value == 'scottishPremiership_select'){
-        document.getElementById('superliga_scores').style.display = 'none';
-        document.getElementById('scottishPremiership_scores').style.display = 'block';
-    }
-    if(document.getElementById('league').value == 'superliga_select'){
-        document.getElementById('scottishPremiership_scores').style.display = 'none';
-        document.getElementById('superliga_scores').style.display = 'block';
-    }
-    if(document.getElementById('league').value == 'all_select'){
-        document.getElementById('scottishPremiership_scores').style.display = 'block';
-        document.getElementById('superliga_scores').style.display = 'block';
-    }
+  //Turn off and on displaying scores depending on user input
+  if (document.getElementById("league").value == "scottishPremiership_select") {
+    document.getElementById("superliga_scores").style.display = "none";
+    document.getElementById("scottishPremiership_scores").style.display =
+      "block";
+  }
+  if (document.getElementById("league").value == "superliga_select") {
+    document.getElementById("scottishPremiership_scores").style.display =
+      "none";
+    document.getElementById("superliga_scores").style.display = "block";
+  }
+  if (document.getElementById("league").value == "all_select") {
+    document.getElementById("scottishPremiership_scores").style.display =
+      "block";
+    document.getElementById("superliga_scores").style.display = "block";
+  }
 
-    date = document.getElementById('date').value;
-    loadData();
+  date = document.getElementById("date").value;
+  loadData();
 }
--- soccerTable.js
+++ soccerTable.js
@@ -2,149 +2,177 @@ let today = new Date();
 let date = today;
 
 //loads data for Scottish Premiership
-function loadData(){
-    let request = new XMLHttpRequest();
+function loadData() {
+  let request = new XMLHttpRequest();
 
-    let api_token = 'CHPO1UMSVjcgZA8oI6xmeLIxzAtVDXeiY6fypOF7npPkB7oqvjniWkg5Z2np';
-    let seasonID = null;
-    console.log(seasonID)
-    console.log(document.getElementById('season').value);
-    do {
-        seasonID = 18369; //ID for current season
-        if (document.getElementById('season').value == '2020/21') { seasonID = 17141}
-        if (document.getElementById('season').value == '2019/20') { seasonID = 16222}
-    } while (seasonID == null)
-    //console.log(date);
-    let url = 'https://soccer.sportmonks.com/api/v2.0/standings/season/' + seasonID + '?api_token=' + api_token;
+  let api_token =
+    "CHPO1UMSVjcgZA8oI6xmeLIxzAtVDXeiY6fypOF7npPkB7oqvjniWkg5Z2np";
+  let seasonID = null;
 
-    request.open('GET', url, true);
-    let table = [];
-
-    request.onload = function() {
-        let response = JSON.parse(this.response);
-        console.log(response);
-
-        if(JSON.stringify(response.data) == []) {
-            console.log('the response is null');
-        }
-
-        let x = response.data[0].standings.data.length;
-        for(let i=0; i<x; i++){
-            table[i] = [
-                JSON.stringify(response.data[0].standings.data[i].position),
-                JSON.stringify(response.data[0].standings.data[i].team_name),
-                JSON.stringify(response.data[0].standings.data[i].overall.points),
-                JSON.stringify(response.data[0].standings.data[i].overall.games_played),
-                JSON.stringify(response.data[0].standings.data[i].overall.won),
-                JSON.stringify(response.data[0].standings.data[i].overall.draw),
-                JSON.stringify(response.data[0].standings.data[i].overall.lost)
-            ];
-            if (seasonID == 16222) {
-                table[i][2] = JSON.stringify(response.data[0].standings.data[i].points);
-            }
-        }
-
-        //Removes "" from strings in array
-        let y = table[0].length;
-        for(let j=0; j<x; j++){
-            for(let k=0; k<y; k++){
-                table[j][k] = table[j][k].replace(/['"]+/g, '');
-            }
-        }
-        console.log('SCOTTISH');
-        console.log(table);
-
-        for(let j=0; j<x; j++){
-            document.getElementById('scottishPremiershipTableHeader').innerHTML = "<div><div class='position'>Pos</div><div class='teamName'>Club</div><div class='tableData'>GP</div><div class='tableData'>W</div><div class='tableData'>D</div><div class='tableData'>L</div><div class='tableData'>Pts</div></div>"
-            document.getElementById('scottishPremiershipTable').innerHTML += "<div id=position_'" + j + "' class='tableRow'><div class='position'>" + table[j][0] + "</div><div class='teamName'>" + table[j][1] + "</div><div class='tableData'>" + table[j][3] + "</div><div class='tableData'>" + table[j][4] + "</div><div class='tableData'>" + table[j][5] + "</div><div class='tableData'>" + table[j][6] + "</div><div class='tableData'>" + table[j][2] + "</div></div>";
-        }
-
+  do {
+    seasonID = 18369; //ID for current season
+    if (document.getElementById("season").value == "2020/21") {
+      seasonID = 17141;
     }
-    request.send();
+    if (document.getElementById("season").value == "2019/20") {
+      seasonID = 16222;
+    }
+  } while (seasonID == null);
+  let url =
+    "https://soccer.sportmonks.com/api/v2.0/standings/season/" +
+    seasonID +
+    "?api_token=" +
+    api_token;
+
+  request.open("GET", url, true);
+  let table = [];
+
+  request.onload = function () {
+    let response = JSON.parse(this.response);
+    let x = response.data[0].standings.data.length;
+    for (let i = 0; i < x; i++) {
+      table[i] = [
+        JSON.stringify(response.data[0].standings.data[i].position),
+        JSON.stringify(response.data[0].standings.data[i].team_name),
+        JSON.stringify(response.data[0].standings.data[i].overall.points),
+        JSON.stringify(response.data[0].standings.data[i].overall.games_played),
+        JSON.stringify(response.data[0].standings.data[i].overall.won),
+        JSON.stringify(response.data[0].standings.data[i].overall.draw),
+        JSON.stringify(response.data[0].standings.data[i].overall.lost),
+      ];
+      if (seasonID == 16222) {
+        table[i][2] = JSON.stringify(response.data[0].standings.data[i].points);
+      }
+    }
+
+    //Removes "" from strings in array
+    let y = table[0].length;
+    for (let j = 0; j < x; j++) {
+      for (let k = 0; k < y; k++) {
+        table[j][k] = table[j][k].replace(/['"]+/g, "");
+      }
+    }
+
+    for (let j = 0; j < x; j++) {
+      document.getElementById("scottishPremiershipTableHeader").innerHTML =
+        "<div><div class='position'>Pos</div><div class='teamName'>Club</div><div class='tableData'>GP</div><div class='tableData'>W</div><div class='tableData'>D</div><div class='tableData'>L</div><div class='tableData'>Pts</div></div>";
+      document.getElementById("scottishPremiershipTable").innerHTML +=
+        "<div id=position_'" +
+        j +
+        "' class='tableRow'><div class='position'>" +
+        table[j][0] +
+        "</div><div class='teamName'>" +
+        table[j][1] +
+        "</div><div class='tableData'>" +
+        table[j][3] +
+        "</div><div class='tableData'>" +
+        table[j][4] +
+        "</div><div class='tableData'>" +
+        table[j][5] +
+        "</div><div class='tableData'>" +
+        table[j][6] +
+        "</div><div class='tableData'>" +
+        table[j][2] +
+        "</div></div>";
+    }
+  };
+  request.send();
 }
 
 //loads data for Danish Superliga
-function loadData_2(){
-    let request = new XMLHttpRequest();
-    
-    let api_token = 'CHPO1UMSVjcgZA8oI6xmeLIxzAtVDXeiY6fypOF7npPkB7oqvjniWkg5Z2np';
-    let seasonID = null;
-    console.log(seasonID)
-    console.log(document.getElementById('season').value);
-    do {
-        seasonID = 18334; //ID for current season
-        if (document.getElementById('season').value == '2020/21') { seasonID = 17328}
-        if (document.getElementById('season').value == '2019/20') { seasonID = 16020}
-    } while (seasonID == null)
-    //console.log(date);
-    let url = 'https://soccer.sportmonks.com/api/v2.0/standings/season/' + seasonID + '?api_token=' + api_token;
-    
-    request.open('GET', url, true);
-    let table = [];
-    
-    request.onload = function() {
-        let response = JSON.parse(this.response);
-        console.log(response);
-        
-        if(JSON.stringify(response.data) == []) {
-            console.log('the response is null');
-        }
-        
-        let x = response.data[0].standings.data.length;
-        for(let i=0; i<x; i++){
-            table[i] = [
-                JSON.stringify(response.data[0].standings.data[i].position),
-                JSON.stringify(response.data[0].standings.data[i].team_name),
-                JSON.stringify(response.data[0].standings.data[i].overall.points),
-                JSON.stringify(response.data[0].standings.data[i].overall.games_played),
-                JSON.stringify(response.data[0].standings.data[i].overall.won),
-                JSON.stringify(response.data[0].standings.data[i].overall.draw),
-                JSON.stringify(response.data[0].standings.data[i].overall.lost)
-            ];
-            if (seasonID == 16020) {
-                table[i][2] = JSON.stringify(response.data[0].standings.data[i].total.points);
-            }
-        }
-        
-        //Removes "" from strings in array
-        let y = table[0].length;
-        for(let j=0; j<x; j++){
-            for(let k=0; k<y; k++){
-                table[j][k] = table[j][k].replace(/['"]+/g, '');
-            }
-        }
-        
-        console.log('SUPERLIGA')
-        console.log(table);
+function loadData_2() {
+  let request = new XMLHttpRequest();
 
-        for(let j=0; j<x; j++){
-            document.getElementById('superligaTableHeader').innerHTML = "<div><div class='position'>Pos</div><div class='teamName'>Club</div><div class='tableData'>GP</div><div class='tableData'>W</div><div class='tableData'>D</div><div class='tableData'>L</div><div class='tableData'>Pts</div></div>"
-            document.getElementById('superligaTable').innerHTML += "<div id=position_'" + j + "' class='tableRow'><div class='position'>" + table[j][0] + "</div><div class='teamName'>" + table[j][1] + "</div><div class='tableData'>" + table[j][3] + "</div><div class='tableData'>" + table[j][4] + "</div><div class='tableData'>" + table[j][5] + "</div><div class='tableData'>" + table[j][6] + "</div><div class='tableData'>" + table[j][2] + "</div></div>";
-        }
+  let api_token =
+    "CHPO1UMSVjcgZA8oI6xmeLIxzAtVDXeiY6fypOF7npPkB7oqvjniWkg5Z2np";
+  let seasonID = null;
+  do {
+    seasonID = 18334; //ID for current season
+    if (document.getElementById("season").value == "2020/21") {
+      seasonID = 17328;
     }
-    request.send();
+    if (document.getElementById("season").value == "2019/20") {
+      seasonID = 16020;
+    }
+  } while (seasonID == null);
+  let url =
+    "https://soccer.sportmonks.com/api/v2.0/standings/season/" +
+    seasonID +
+    "?api_token=" +
+    api_token;
+  request.open("GET", url, true);
+  let table = [];
+  request.onload = function () {
+    let response = JSON.parse(this.response);
+    let x = response.data[0].standings.data.length;
+    for (let i = 0; i < x; i++) {
+      table[i] = [
+        JSON.stringify(response.data[0].standings.data[i].position),
+        JSON.stringify(response.data[0].standings.data[i].team_name),
+        JSON.stringify(response.data[0].standings.data[i].overall.points),
+        JSON.stringify(response.data[0].standings.data[i].overall.games_played),
+        JSON.stringify(response.data[0].standings.data[i].overall.won),
+        JSON.stringify(response.data[0].standings.data[i].overall.draw),
+        JSON.stringify(response.data[0].standings.data[i].overall.lost),
+      ];
+      if (seasonID == 16020) {
+        table[i][2] = JSON.stringify(
+          response.data[0].standings.data[i].total.points
+        );
+      }
+    }
+
+    //Removes "" from strings in array
+    let y = table[0].length;
+    for (let j = 0; j < x; j++) {
+      for (let k = 0; k < y; k++) {
+        table[j][k] = table[j][k].replace(/['"]+/g, "");
+      }
+    }
+    for (let j = 0; j < x; j++) {
+      document.getElementById("superligaTableHeader").innerHTML =
+        "<div><div class='position'>Pos</div><div class='teamName'>Club</div><div class='tableData'>GP</div><div class='tableData'>W</div><div class='tableData'>D</div><div class='tableData'>L</div><div class='tableData'>Pts</div></div>";
+      document.getElementById("superligaTable").innerHTML +=
+        "<div id=position_'" +
+        j +
+        "' class='tableRow'><div class='position'>" +
+        table[j][0] +
+        "</div><div class='teamName'>" +
+        table[j][1] +
+        "</div><div class='tableData'>" +
+        table[j][3] +
+        "</div><div class='tableData'>" +
+        table[j][4] +
+        "</div><div class='tableData'>" +
+        table[j][5] +
+        "</div><div class='tableData'>" +
+        table[j][6] +
+        "</div><div class='tableData'>" +
+        table[j][2] +
+        "</div></div>";
+    }
+  };
+  request.send();
 }
 
-
 function submitchange() {
-    
-    document.getElementById('scottishPremiershipTable').innerHTML = '';
-    document.getElementById('superligaTable').innerHTML = '';
+  document.getElementById("scottishPremiershipTable").innerHTML = "";
+  document.getElementById("superligaTable").innerHTML = "";
 
-    //Turn off and on displaying scores depending on user input
-    if(document.getElementById('league').value == 'scottishPremiership_select'){
-        document.getElementById('superliga').style.display = 'none';
-        document.getElementById('scottishPremiership').style.display = 'block';
-    }
-    if(document.getElementById('league').value == 'superliga_select'){
-        document.getElementById('scottishPremiership').style.display = 'none';
-        document.getElementById('superliga').style.display = 'block';
-    }
-    if(document.getElementById('league').value == 'all_select'){
-        document.getElementById('scottishPremiership').style.display = 'block';
-        document.getElementById('superliga').style.display = 'block';
-    }
+  //Turn off and on displaying scores depending on user input
+  if (document.getElementById("league").value == "scottishPremiership_select") {
+    document.getElementById("superliga").style.display = "none";
+    document.getElementById("scottishPremiership").style.display = "block";
+  }
+  if (document.getElementById("league").value == "superliga_select") {
+    document.getElementById("scottishPremiership").style.display = "none";
+    document.getElementById("superliga").style.display = "block";
+  }
+  if (document.getElementById("league").value == "all_select") {
+    document.getElementById("scottishPremiership").style.display = "block";
+    document.getElementById("superliga").style.display = "block";
+  }
 
-    loadData();
-    loadData_2();
+  loadData();
+  loadData_2();
 }
